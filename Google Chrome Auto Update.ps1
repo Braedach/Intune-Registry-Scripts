2ad7a46f5c73806336a71ddd4255452b671db571
Braedach <40484379+Braedach@users.noreply.github.com>
2021-02-04 08:16:46 +0800

    Update Google Chrome Auto Update.ps1
    Wrong value recommended by Microsoft WDATP
    Investigating ADMX implementation in lieu
    This script has been updated but deallocated in Intune for the time being

--- Google Chrome Auto Update.ps1
+++ Google Chrome Auto Update.ps1
@@ -7,7 +7,7 @@ If (Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update')
     {
     #If the key already exists just set the value
     #Write-Output "True"
-    Set-Itemproperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'Updatedefault' -value '1'
+    Set-Itemproperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'Updatedefault' -value '3'
     Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name Updatedefault
     }
     else
@@ -15,7 +15,7 @@ If (Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update')
      ##If the key doesnt exist then the program is not installed and doesnt need rectification
      Write-Output "False"
      New-Item -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update\' -Force
-     New-Item -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'Updatedefault' -value '1'
+     New-Item -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'Updatedefault' -value '3'
      Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name Updatedefault
      }
 
