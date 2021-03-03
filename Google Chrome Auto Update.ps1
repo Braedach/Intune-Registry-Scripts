56495b362c00f411074b25497483864ada0e3239
Braedach <40484379+Braedach@users.noreply.github.com>
2021-03-03 18:36:48 +0800

    Update Google Chrome Auto Update.ps1
    Reinstated the script and modified it

--- Google Chrome Auto Update.ps1
+++ Google Chrome Auto Update.ps1
@@ -1,22 +1,20 @@
 ﻿#Setup Auto update on Google Chrome
 #Call the registry value to see if it exists, if it does modify it, if it doesnt create it.
-#This control has been depreciated and removed from Intune.  ADMX control is used in lieu
-#Updated 04-02-2021
+#Setup Auto update on Google Chrome
+#Test to see if the key exists, if it does change it, if it doesnt do nothing - not installed
+#Updated 03-03-2021
 
 
-If (Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update')
+If (Test-Path -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update')
     {
     #If the key already exists just set the value
     #Write-Output "True"
     Set-Itemproperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'Updatedefault' -value '3'
-    Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name Updatedefault
+    Set-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'AutoUpdateCheckPeriod' -value '180'
+    Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update'
     }
     else
     {
      ##If the key doesnt exist then the program is not installed and doesnt need rectification
      Write-Output "False"
-     New-Item -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update\' -Force
-     New-Item -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name 'Updatedefault' -value '3'
-     Get-ItemProperty -Path 'Registry::HKLM\SOFTWARE\Policies\Google\Update' -Name Updatedefault
      }
-
