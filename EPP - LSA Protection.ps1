172a95378be4592c5ae4f26cb6aab046a9df2126
Braedach <40484379+Braedach@users.noreply.github.com>
2021-03-05 16:30:38 +0800

    Update EPP - LSA Protection.ps1
    Complete rewrite
    Successful deployment 20210503-150000
    Need to watch for audit policy changes on joined machines
    If not successful use PowerShell script to force the issue across all devices registered and joined.

--- EPP - LSA Protection.ps1
+++ EPP - LSA Protection.ps1
@@ -1,6 +1,18 @@
-#EPP Security recommendation
-#Call the registry value
-Get-ItemProperty -Path Registry::HKLM\SYSTEM\CurrentControlSet\Control\Lsa\ -Name RunAsPPL
+#Fix LSA for joined machines and set prerequisites for advanced auditing controls
+#Create or modify the registry keys no matter what
+#Test machine had second key - other test machine did not have second key and is required.
+#created based on research 05-03-2021
 
-#Set the registry value
-Set-Itemproperty -Path Registry::HKLM\SYSTEM\CurrentControlSet\Control\Lsa\ -Name 'RunAsPPL' -value '1'
+
+If (Test-path -Path 'Registry::HKLM\SYSTEM\CurrentControlSet\Control\Lsa\')
+    {
+    #If the key already exists just set the value
+    Write-Output "True"
+    Set-Itemproperty -Path Registry::HKLM\SYSTEM\CurrentControlSet\Control\Lsa\ -Name 'RunAsPPL' -value '1'
+    Set-ItemProperty -Path Registry::HKLM\SYSTEM\CurrentControlSet\Control\Lsa\ -Name 'SCENoApplyLegacyAuditPolicy' -value '1'
+    }
+    else
+    {
+    Write-Output "False"
+    }
+
